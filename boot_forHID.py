ef8e09f181d16bd7d5fb666f48b62963a362118c
Promethees <tqmthong@gmail.com>
2025-06-05 09:47:42 +0700

    Readd boot_forHID.py

---
+++ boot_forHID.py
@@ -0,0 +1,12 @@
+import usb_cdc
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+# Disable USB CDC to prevent read-only filesystem
+# usb_cdc.disable()
+
+# Enable USB HID with keyboard device
+keyboard = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(keyboard)
+usb_hid.enable((keyboard,))
