a35a2b3c97f28d16eb715bd368aa85466299860e
Promethees <tqmthong@gmail.com>
2025-07-09 13:59:37 +0700

    Fix bug: Concentration is None cannot be saved

--- src/button_handler.py
+++ src/button_handler.py
@@ -178,10 +178,11 @@ class ButtonHandler:
     def _handle_concentration_mode(self, buttons):
         if self.menu_button_pressed(buttons):
             self.colorimeter.concentration = self.colorimeter.screen_manager.get_concentration_value()
+            self.colorimeter.to_use_gain_asB = False
             if self.colorimeter.concentration is not None:
-                self.colorimeter.to_use_gain_asB = False
                 self.colorimeter.screen_manager.show_message(f"Concentration saved: {self.colorimeter.concentration} nM/l", is_error=False)
-            
+            else:
+                self.colorimeter.screen_manager.show_message(f"Concentration saved: None nM/l", is_error=False)
         elif self.blank_button_pressed(buttons):
             self.colorimeter.screen_manager.set_concentration_to_zero()
         elif self.up_button_pressed(buttons):
